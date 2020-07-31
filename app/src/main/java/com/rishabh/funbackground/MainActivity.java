6c8f3596805aeffeb1b8c6d880a0b261325a6e46
RISHABH MISHRA <dmrishabh@users.noreply.github.com>
2020-07-31 19:06:17 +0530

    first version committed on master branch

---
+++ app/src/main/java/com/rishabh/funbackground/MainActivity.java
@@ -0,0 +1,38 @@
+package com.rishabh.funbackground;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout layout;
+    private ImageButton refreshBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout=findViewById(R.id.myLayout);
+        refreshBtn= findViewById(R.id.imageButton);
+
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random(); // this is a predefined method which bring random numbers for us
+
+                int color = Color.argb(255,random.nextInt(256),random.nextInt(256),random.nextInt(256));
+
+                layout.setBackgroundColor(color);
+            }
+        });
+
+    }
+}
